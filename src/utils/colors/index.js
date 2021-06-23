459ed603c3582876c9d08d2026b1daaa5e1c3be2
Bali Algoritm <putualgoritma@gmail.com>
2021-06-23 21:38:00 +0800

    Up 01

---
+++ src/utils/colors/index.js
@@ -0,0 +1,12 @@
+export const colors = {
+  default: '#DBA514',
+  disable: '#cfd3ce',
+  dark: '#474747',
+  btn: '#ff781f',
+  borderColor: '#bbbbbb',
+  text: {
+    default: '#ffffff',
+    black: '#1c2b2d',
+    grey: '#394867',
+  },
+};
